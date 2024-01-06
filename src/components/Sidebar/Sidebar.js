8d733217e490fe19f9315288ac20c2af9bd51f44
david waza <idemetoemediong2022@gmail.com>
2024-01-07 00:37:30 +0100

    created sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,98 @@
+"use client";
+import { ChevronFirst, ChevronLast, LogOut } from "lucide-react";
+import { createContext, useContext, useState } from "react";
+
+const SidebarContext = createContext();
+export const Sidebar = ({ children }) => {
+  const [expanded, setExpanded] = useState(true);
+  return (
+    <>
+      <aside
+        className={`h-screen transition-all ${
+          expanded ? "w-[300px]" : "w-[80px]"
+        }`}
+      >
+        <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+          <div className="p-4 pb-7 flex justify-between items-center">
+            <img
+              src="https://img.logoipsum.com/243.svg"
+              alt="logo"
+              className={`overflow-hidden transition-all ${
+                expanded ? "w-32" : "w-0"
+              }`}
+            ></img>
+            <button
+              className="p-1 rounded-lg bg-gray-50 hover:bg-gray-100"
+              onClick={() => setExpanded((curr) => !curr)}
+            >
+              {expanded ? (
+                <ChevronFirst className="text-black" />
+              ) : (
+                <ChevronLast className="text-black" />
+              )}
+            </button>
+          </div>
+          <SidebarContext.Provider value={{ expanded }}>
+            <ul className="flex-1 px-3">{children}</ul>
+          </SidebarContext.Provider>
+          <div
+            className={`overflow-hidden transition-all ${
+              expanded ? "" : "w-0"
+            }`}
+          >
+            <div className={`border-t flex p-3 `}>
+              <LogOut className="text-black" size={20} />
+              <h3 className={` text-black ml-3`}>Log out</h3>
+            </div>
+          </div>
+        </nav>
+      </aside>
+    </>
+  );
+};
+
+export const SidebarItems = ({ icon, text, active, alert }) => {
+  const { expanded } = useContext(SidebarContext);
+  return (
+    <>
+      <li
+        className={`relative flex items-center py-2 px-3 my-1 
+        font-medium rounded-md cursor-pointer 
+        transition-colors group ${
+          active
+            ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+            : "hover:bg-indigo-50 text-gray-600"
+        }
+        `}
+      >
+        {icon}
+        <span
+          className={`overflow-hidden transition-all ${
+            expanded ? "w-52 ml-3" : "w-0"
+          }`}
+        >
+          {text}
+        </span>
+        {alert && (
+          <div
+            className={`absolute right-2 w-2 h-2 rounded-md bg-indigo-400 ${
+              expanded ? "" : "top-2"
+            }`}
+          ></div>
+        )}
+
+        {!expanded && (
+          <div
+            className="absolute left-full rounded-md px-2 py-1 ml-6 
+          bg-indigo-100 text-indigo-800 text-sm 
+          invisible opacity-20 -translate-x-3 transition-all
+          group-hover:visible group-hover:opacity-100 
+          group-hover:translate-x-0"
+          >
+            {text}
+          </div>
+        )}
+      </li>
+    </>
+  );
+};
